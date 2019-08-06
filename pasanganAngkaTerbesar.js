ed0a9c4015083e3e05daab7658eba30987a092d7
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-08-07 00:28:30 +0700

    Pasangan Terbesar Angka

---
+++ pasanganAngkaTerbesar.js
@@ -0,0 +1,23 @@
+function pasanganTerbesar(num) {
+  var array = String(num).split("");
+  //   console.log(array);
+  var hasil = [];
+  for (var i = 0; i < array.length - 1; i++) {
+    hasil[i] = array[i] + array[i + 1];
+  }
+  var output = hasil[0];
+  console.log(hasil);
+  for (var j = 0; j < hasil.length; j++) {
+    if (hasil[j] > output) {
+      output = hasil[j];
+    }
+  }
+  return output;
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
